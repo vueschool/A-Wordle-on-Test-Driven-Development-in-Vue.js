c80400e989b69fec6a3daf35ae3ad6f05e362513
Felipe Flor <hello@felipeflor.com>
2023-12-08 04:07:28 -0500

    Remove clutter

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,15 +3,12 @@ import WordleBoard from "../WordleBoard.vue"
 
 describe("WordleBoard", () => {
   test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
-    // Arrange
     const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
 
-    // Act
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
 
-    // Assert
     expect(wrapper.text()).toContain("You won!")
   })
 })
