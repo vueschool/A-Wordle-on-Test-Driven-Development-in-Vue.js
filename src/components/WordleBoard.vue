3d2e8c2615ff493b95d8753cadf198e25943dd67
Felipe Flor <hello@felipeflor.com>
2023-12-08 05:00:24 -0500

    Only render end-of-game feedback after the game is over

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import {DEFEAT_MESSAGE, VICTORY_MESSAGE} from "@/settings"
+import {ref} from "vue"
 
-defineProps<{
-  wordOfTheDay: string
-}>()
+defineProps<{ wordOfTheDay: string }>()
+
+const guessInProgress = ref("")
+const guessSubmitted = ref("")
 </script>
 
 <template>
-  <input type="text">
-  <p>{{ VICTORY_MESSAGE }}</p>
-  <p>{{ DEFEAT_MESSAGE }}!</p>
+  <input v-model="guessInProgress" type="text" @keydown.enter="guessSubmitted = guessInProgress">
+  <p v-if="guessSubmitted.length > 0"
+     v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"/>
 </template>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -24,5 +24,10 @@ describe("WordleBoard", () => {
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
-  test.todo("no end-of-game message appears if the user has not yet made a guess")
+  test("no end-of-game message appears if the user has not yet made a guess", async () => {
+    const wrapper = mount(WordleBoard, {props: {wordOfTheDay}})
+
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+  })
 })
