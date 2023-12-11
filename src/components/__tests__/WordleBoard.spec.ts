8172dfb9826b6dd717ef179237d6f633a70137c8
Felipe Flor <hello@felipeflor.com>
2023-12-11 01:24:44 -0500

    Ensure no warnings are rendered when a proper word of the day is provided

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -57,4 +57,12 @@ describe("WordleBoard", () => {
 
         expect(console.warn).toHaveBeenCalled()
     })
+
+    test("no warning is emitted if the word of the day provided is a real uppercase English word with 5 characters", async () => {
+        console.warn = vi.fn()
+
+        mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+
+        expect(console.warn).not.toHaveBeenCalled()
+    })
 })
