45119c397ec808ce58758ec2e85dd7df5f02bdba
Felipe Flor <hello@felipeflor.com>
2023-12-15 00:38:30 -0500

    Move mock of console.warn to beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -37,6 +37,10 @@ describe("WordleBoard", () => {
     })
 
     describe("Rules for defining the word of the day", () => {
+        beforeEach(() => {
+            console.warn = vi.fn()
+        })
+
         test.each(
             [
                 {wordOfTheDay: "FLY", reason: "word-of-the-day must have 5 characters"},
@@ -44,16 +48,12 @@ describe("WordleBoard", () => {
                 {wordOfTheDay: "QWERT", reason: "word-of-the-day must be a valid English word"}
             ]
         )("Since $reason: $wordOfTheDay is invalid, therefore a warning must be emitted", async ({wordOfTheDay}) => {
-            console.warn = vi.fn()
-
             mount(WordleBoard, {props: {wordOfTheDay}})
 
             expect(console.warn).toHaveBeenCalled()
         })
 
         test("no warning is emitted if the word of the day provided is a real uppercase English word with 5 characters", async () => {
-            console.warn = vi.fn()
-
             mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
 
             expect(console.warn).not.toHaveBeenCalled()
