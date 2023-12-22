ec241790f1c0c6f2a456fe5d0d9b669cf2d160bd
Felipe Flor <hello@felipeflor.com>
2023-12-22 00:02:02 -0500

    Use word of the day that passes validation

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@ import WordleBoard from "@/components/WordleBoard.vue"
 </script>
 
 <template>
-  <wordle-board word-of-the-day="You did it!"/>
+  <wordle-board word-of-the-day="TESTS"/>
 </template>
