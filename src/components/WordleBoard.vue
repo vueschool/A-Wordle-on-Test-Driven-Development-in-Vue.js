3c8b0f3b1b2cb30b75f23fe72d7757395f7115a4
Felipe Flor <hello@felipeflor.com>
2024-01-09 08:38:37 -0500

    Make WordleBoard prettier

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -15,8 +15,38 @@ const guessSubmitted = ref("")
 </script>
 
 <template>
-  <guess-input @guess-submitted="guess => guessSubmitted = guess"/>
+  <main>
+    <guess-input @guess-submitted="guess => guessSubmitted = guess"/>
 
-  <p v-if="guessSubmitted.length > 0"
-     v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"/>
+    <p v-if="guessSubmitted.length > 0"
+       class="end-of-game-message"
+       v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"/>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+}
+
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
