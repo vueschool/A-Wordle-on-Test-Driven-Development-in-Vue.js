e6a4c2585783bd223a8852b0655dc069eb625ce9
Felipe Flor <hello@felipeflor.com>
2024-01-09 08:36:52 -0500

    Make GuessInput prettier

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -33,6 +33,14 @@ function onSubmit() {
 </script>
 
 <template>
+  <ul class="word">
+    <li v-for="(letter, index) in formattedGuessInProgress.padEnd(WORD_SIZE, ' ')"
+        :key="`${letter}-${index}`"
+        :data-letter="letter"
+        class="letter"
+        v-text="letter"/>
+  </ul>
+
   <input v-model="formattedGuessInProgress"
          :maxlength="WORD_SIZE"
          autofocus
@@ -40,3 +48,43 @@ function onSubmit() {
          type="text"
          @keydown.enter="onSubmit">
 </template>
+
+<style scoped>
+input {
+  position: absolute;
+  opacity: 0;
+}
+
+.word {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.25rem;
+}
+
+.letter {
+  background-color: white;
+  border: 1px solid hsl(0, 0%, 70%);
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bolder;
+}
+
+li:not([data-letter=" "]) {
+  animation: pop 100ms;
+}
+
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.4);
+  }
+}
+</style>
