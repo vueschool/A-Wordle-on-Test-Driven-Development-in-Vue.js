d1d3621862c0788701f3840fbe5791689b137ba8
Felipe Flor <hello@felipeflor.com>
2024-02-04 19:27:20 -0500

    Extract constants

--- src/components/GuessView.vue
+++ src/components/GuessView.vue
@@ -8,11 +8,14 @@ function getFeedback(letterPosition: number): null | "correct" | "incorrect" | "
     return null
   }
 
-  if (!props.answer.includes(props.guess[letterPosition])) {
+  const letterGuessed = props.guess[letterPosition]
+  const letterExpected = props.answer[letterPosition]
+
+  if (!props.answer.includes(letterGuessed)) {
     return "incorrect"
   }
 
-  return props.answer[letterPosition] === props.guess[letterPosition] ? "correct" : "almost"
+  return letterExpected === letterGuessed ? "correct" : "almost"
 }
 </script>
 
